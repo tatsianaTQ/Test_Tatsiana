1e4926cfa152e3b24cd912c31ab9a2c143198e41
tatsianaTQ <tshamshurina@telequebec.tv>
2025-10-01 22:10:20 -0400

    Update selenium_setup.py

--- src/common/selenium_setup.py
+++ src/common/selenium_setup.py
@@ -7,8 +7,9 @@ def new_driver():
     opts.add_argument("--window-size=1280,1024")
     opts.add_argument("--disable-blink-features=AutomationControlled")
     opts.add_argument("--headless=new")
-        opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--disable-dev-shm-usage")
     # -----------------
     opts.add_experimental_option("excludeSwitches", ["enable-automation"])
     opts.add_experimental_option("useAutomationExtension", False)
     return webdriver.Chrome(options=opts)
+
